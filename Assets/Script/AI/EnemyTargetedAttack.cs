9cf40b55d2288bf896ec793c21ce4fd0e651dfc6
Devan Farmer <devanf21@gmail.com>
2025-09-26 15:35:27 +0200

    Created Targeted Attack script

---
+++ Assets/Script/AI/EnemyTargetedAttack.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class EnemyTargetedAttack : MonoBehaviour
+{
+    [SerializeField] bool attackEnabled;
+
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float minAttackSpeed;
+    [SerializeField] float maxAttackSpeed;
+    [SerializeField] Transform target;
+
+    float attackSpeed;
+
+    float lastAttackTime;
+
+    private void Start()
+    {
+        SetAttackSpeed();
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+
+        if (lastAttackTime + attackSpeed <= Time.time)
+        {
+            Attack();
+            SetAttackSpeed();
+            lastAttackTime = Time.time;
+        }
+    }
+
+    void SetAttackSpeed()
+    {
+        attackSpeed = Random.Range(minAttackSpeed, maxAttackSpeed);
+    }
+
+    void Attack()
+    {
+        GameObject proj = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+        proj.GetComponent<ProjectileComponent>().SetDirection((target.position - transform.position).normalized);
+    }
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    public void SetAttackEnabled(bool enabled)
+    {
+        attackEnabled = enabled;
+    }
+}
