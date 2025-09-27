10cc4fc1d1e11d34702121cc5a2ee7df17b9fe02
Devan Farmer <devanf21@gmail.com>
2025-09-27 16:13:08 +0200

    Added attack enabled functionality

--- Assets/Script/AI/EnemyTargetedAttack.cs
+++ Assets/Script/AI/EnemyTargetedAttack.cs
@@ -24,7 +24,7 @@ public class EnemyTargetedAttack : MonoBehaviour
 
         if (lastAttackTime + attackSpeed <= Time.time)
         {
-            Attack();
+            if (attackEnabled) Attack();
             SetAttackSpeed();
             lastAttackTime = Time.time;
         }
